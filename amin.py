eb806b5fb0b29bbe683572c9855b2e28c815b9f0
aminhp93 <minhpn.org.ec@gmail.com>
2016-09-06 11:21:28 -0700

    string

---
+++ amin.py
@@ -0,0 +1,29 @@
+print "this is a sample string"
+name = "Zen"
+print "my name is " + name
+first_name = "Zen"
+last_name = "coder"
+print "My nameis {} {}".format(first_name, last_name)
+
+my_string = 'hello world'
+print my_string.capitalize()
+
+my_string = "HELO WEOflD"
+print my_string.lower()
+
+my_string = "Hello WORLD"
+print my_string.swapcase()
+
+my_string = 'hello world'
+print my_string.upper()
+
+my_string = "hello world"
+print my_string.find("e l")
+
+my_string = "hello world"
+print my_string.replace("world", "kitty")
+
+my_string = "egg, egg, Spam, egg and Spam"
+print my_string.replace("egg", "Spam", 2)
+
+
